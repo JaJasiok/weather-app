dba33156352db061d5abd449b724e84286fad96f
JaJasiok <jasiek.k01@gmail.com>
2023-08-19 13:59:23 +0200

    Forced dark mode

--- app/src/main/java/com/example/weatherapp/WeatherApplication.kt
+++ app/src/main/java/com/example/weatherapp/WeatherApplication.kt
@@ -1,6 +1,7 @@
 package com.example.weatherapp
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.weatherapp.db.LocationDatabase
 import com.example.weatherapp.db.LocationRepository
 
@@ -8,4 +9,9 @@ class WeatherApplication : Application() {
 
     private val database by lazy { LocationDatabase.getDatabase(this) }
     val repository by lazy { LocationRepository(database.locationDao()) }
+
+    override fun onCreate() {
+        super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+    }
 }
